359ebb0eb3e925decd4855a9b8ee3df51d99504f
Davi Freitas <davimello14@gmail.com>
2024-07-28 16:59:57 -0300

    [Add] finish get basket endpoint

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndPoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndPoint.cs
@@ -1,6 +1,23 @@
 ﻿namespace Basket.API.Basket.GetBasket;
 
-public class GetBasketEndPoint
+public record GetBasketResponse(ShoppingCart Cart);
+
+public class GetBasketEndPoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                GetBasketResult getBasketResult = await sender.Send(new GetBasketQuery(userName));
+
+                var getBasketResponse = new GetBasketResponse(getBasketResult.Cart);
+
+                return Results.Ok(getBasketResponse);
+            })
+            .WithName("Get basket by userName")
+            .Produces<GetBasketResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get basket by userName")
+            .WithDescription("Get basket by userName");
+    }
 }
