7c40ede59657e56b4a395e8667d2f23d5b3d3cd8
Davi Freitas <davimello14@gmail.com>
2024-07-28 17:00:14 -0300

    [Feat] store basket endpoint first implementation

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndPoint.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndPoint.cs
@@ -1,6 +1,26 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
 
-public class StoreBasketEndPoint
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketRequest(ShoppingCart Cart);
+
+public record StoreBasketResponse(string UserName);
+
+public class StoreBasketEndPoint : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
+            {
+                var result = await sender.Send(new StoreBasketCommand(request.Cart));
+                var response = new StoreBasketResponse(result.userName);
+
+                return Results.Ok(response);
+            })
+            .WithName("Store basket")
+            .WithDescription("store basket and return username")
+            .WithSummary("Store basket")
+            .Produces<StoreBasketResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,22 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketHandler
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string userName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
-    
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart field is required");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("username is required");
+    }
+}
+public class StoreBasketHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart shoppingCartForCreate = command.Cart;
+        return new StoreBasketResult("swm");
+    }
 }
