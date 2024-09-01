87e8b565a375b3c352793c608da855ffb50675bc
Davi Freitas <davimello14@gmail.com>
2024-09-01 17:12:38 -0300

    [Feat] create order command and handler

---
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Ordering.Domain.Enums;
+global using Ordering.Domain.Models;
+global using BuildingBlocks.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.DTO;
+global using Ordering.Domain.ValueObjects;
---
+++ src/Services/Ordering/Ordering.Application/Orders/Command/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,38 @@
+﻿namespace Ordering.Application.Orders.Command.CreateOrder;
+
+public class CreateOrderHandler(IApplicationDbContext dbContext) : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public async Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        Order order = CreateNewOrder(request.Order);
+
+        dbContext.Orders.Add(order);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new CreateOrderResult(order.Id.Value);
+    }
+
+    private Order CreateNewOrder(OrderDto orderDto)
+    {
+        var shippingAddressForNewOrder = Address.Of(orderDto.ShippingAddress.FirstName, orderDto.ShippingAddress.LastName,
+            orderDto.ShippingAddress.EmailAddress, orderDto.ShippingAddress.AddressLine,
+            orderDto.ShippingAddress.Country, orderDto.ShippingAddress.State, orderDto.ShippingAddress.ZipCode);
+
+        var billingAddressForNewOrder = Address.Of(orderDto.BillingAddress.FirstName, orderDto.BillingAddress.LastName,
+            orderDto.BillingAddress.EmailAddress, orderDto.BillingAddress.AddressLine, orderDto.BillingAddress.Country,
+            orderDto.BillingAddress.State, orderDto.BillingAddress.ZipCode);
+
+        var paymentForNewOrder = Payment.Of(orderDto.Payment.CardNumber, orderDto.Payment.CardHolderName, orderDto.Payment.Expiration,
+            orderDto.Payment.Cvv, orderDto.Payment.PaymentMethod);
+
+        var newOrder = Order.Create(OrderId.Of(Guid.NewGuid()), CustomerId.Of(orderDto.CustomerId), OrderName.Of(orderDto.OrderName),
+            shippingAddressForNewOrder, billingAddressForNewOrder, paymentForNewOrder);
+
+        foreach (var orderItemDto in orderDto.OrderItems)
+        {
+            newOrder.Add(ProductId.Of(orderItemDto.ProductId), orderItemDto.Quantity, orderItemDto.Price);
+        }
+
+        return newOrder;
+    }
+}
